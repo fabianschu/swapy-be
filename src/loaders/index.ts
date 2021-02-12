7d19578fa1260fcf16a4d1c1290721995f6314d8
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2021-02-12 10:56:11 +0100

    remvoes sequelize

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,10 +1,10 @@
 import expressLoader from "./express";
 import dependencyInjectorLoader from "./dependencyInjector";
 import Logger from "./logger";
-import postgresLoader from "./postgres";
+// import postgresLoader from "./postgres";
 
 export default async ({ expressApp }) => {
-  postgresLoader();
+  // postgresLoader();
   Logger.info("✌️ DB loaded and connected!");
 
   dependencyInjectorLoader();
--- src/loaders/postgres.ts
+++ src/loaders/postgres.ts
@@ -1,7 +1,7 @@
-import { Sequelize } from "sequelize-typescript";
-import config from "../config";
+// import { Sequelize } from "sequelize-typescript";
+// import config from "../config";
 
-export default async () => {
-  const sequelize = new Sequelize(config.databaseURL);
-  return sequelize;
-};
+// export default async () => {
+//   const sequelize = new Sequelize(config.databaseURL);
+//   return sequelize;
+// };
